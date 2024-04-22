e7dfcc17cbbbb8156e261953302fa8e65795ab94
Simon Fetzel <simonf93@univie.ac.at>
2024-04-23 01:03:25 +0200

    Added example with two layers where the first layer has an activation function but second doesn't.

--- HessianTanhOneActivationTest2.py
+++ HessianTanhOneActivationTest2.py
@@ -1,6 +1,6 @@
 """
 Verifies the Hessian of a two layer neural network
-where the first layer has an activation function but the second doesn't.
+where the second layer has an activation function but the first doesn't.
 """
 import torch
 from torch import tensor, randn, kron, eye, diag, diag_embed
@@ -28,7 +28,7 @@ mat1 = randn((3, 2), requires_grad=True)
 
 mat2 = tensor([[7., 9.],
                [8., 10.]], requires_grad=True)
-#mat2 = randn((2, 1), requires_grad=True)
+mat2 = randn((2, 2), requires_grad=True)
 
 mat3 = tensor([[1.], [1.]])
 
@@ -41,7 +41,7 @@ def layer2(x, mat2):
     return x @ unvectorize(mat2, (2, 2))
 
 def model(mat1, mat2):
-    return layer2(activation_f(layer1(x, mat1)), mat2) @ mat3
+    return activation_f(layer2(layer1(x, mat1), mat2)) @ mat3
 
 
 inputs = (vectorize(mat1), vectorize(mat2))
@@ -49,32 +49,33 @@ out = model(*inputs)
 out.backward()
 
 out_layer1 = layer1(x, inputs[0])
-activations_layer1 = activation_f(out_layer1)
-out_layer2 = layer2(activations_layer1, inputs[1])
+out_layer2 = layer2(out_layer1, inputs[1])
+activations_2 = activation_f(out_layer2)
 
 # second layer gradient
 print("\nSecond layer gradient")
 print(mat2.grad)
-print(activations_layer1.T @ mat3.T)
+print(out_layer1.T @ pointwise(mat3.T, activation_f_diff(out_layer2)))
 
 # first layer gradient
 print("\nFirst layer gradient")
 print(mat1.grad)
-print(x.t() @ (pointwise(mat3.t() @ mat2.t(), activation_f_diff(out_layer1))))
+print(x.t() @ (pointwise(mat3.t(), activation_f_diff(out_layer2)) @ mat2.t()))
 
 hessian_l = hessian(model, inputs)
 # second layer hessian
 print("\nSecond layer hessian")
-# zero because there is no activation function.
 print(hessian_l[1][1])
+print(kron2(eye(2), out_layer1.t()) @ diag(vectorize(pointwise(mat3.t(), activation_f_diff2(out_layer2)))) @ kron2(eye(2), out_layer1))
+
 
 # first layer hessian
 # derivative of inputs[1].t() * activation_f_diff(out_layer1) * x.t()
 print("\nFirst layer hessian")
 print(hessian_l[0][0])
 
-activation_diag = diag(vectorize(mat3.t() @ mat2.t()) * vectorize(activation_f_diff2(out_layer1)))
-
-print(kron2(eye(2), x.t()) @ activation_diag @ kron(eye(2), x))
+h0_diag = diag(vectorize(pointwise(mat3.t(), activation_f_diff2(out_layer2))))
+# using: d/dW1 (X*W1*W2) = (W2^T kron x)
+print(kron2(eye(2), x.t()) @ kron2(mat2, eye(1)) @ h0_diag @ kron2(mat2.t(), x))
 
 
